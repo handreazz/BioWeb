52ec99bd2e90a5a73897f5a2874328b6b30a038f
zjuchenyuan <chenyuan@zju.edu.cn>
2017-03-05 20:57:03 +0800

    EasyLogin: New version

--- Lib/EasyLogin.py
+++ Lib/EasyLogin.py
@@ -3,18 +3,6 @@
 # TODO:
 #   support for json object post; support file upload
 #   more document is also needed
-# UPDATE_LOG:
-#  2016/12/16
-#    Using PyCharm to write better code
-#  2016/12/15
-#    BeautifulSoup using "content" rather than "text" which requests returned
-#  2016/12/02
-#    adding w function; multiple User-Agent choice; proxies applied to https, get function can have a cachefile
-#  2016/11/25
-#    adding VIEWSTATE,save,load function
-#  2016/10/28
-#    "result=True" by default, delete "from_encoding=x.encoding"
-
 
 try:
     from urllib.parse import urlencode, quote
@@ -26,6 +14,9 @@ import pickle
 import os
 import random
 from bs4 import BeautifulSoup
+import hashlib
+from collections import OrderedDict
+import json
 
 UALIST = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
@@ -43,29 +34,43 @@ UALIST = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36"]
 
+def mymd5(input):
+    return hashlib.md5(bytes(input,encoding="utf-8")).hexdigest()
 
 class EasyLogin:
-    def __init__(self, cookie=None, cookiefile=None, proxy=None):
+    def __init__(self, cookie=None, cookiestring=None, cookiefile=None, proxy=None, session=None):
         """
         example: a = EasyLogin(cookie={"PHPSESSID":"..."}, proxy="socks5://127.0.0.1:1080")
         :param cookie: a dict of cookie
         :param cookiefile: the file contain cookie which saved by get or post(save=True)
         :param proxy: the proxy to use, rememeber schema and `pip install requests[socks]`
         """
-        if cookie is None:
-            cookie = {}
         self.b = None
-        self.s = requests.Session()
-        self.s.headers.update({'User-Agent': random.choice(UALIST)})
-        self.s.cookies.update(cookie)
         self.proxies = {'http': proxy, 'https': proxy} if proxy is not None else None
         self.cookiefile = 'cookie.pickle'
+        if session is not None:
+            self.s = session
+            return
+        self.s = requests.Session()
+        self.s.headers.update({'User-Agent': random.choice(UALIST)})
+        if cookie is None:
+            cookie = {}
+        self.s.cookies.update(cookie)
+        self.setcookie(cookiestring)
         if cookiefile is not None:
             self.cookiefile = cookiefile
             try:
                 self.s.cookies = pickle.load(open(cookiefile, "rb"))
             except FileNotFoundError:
                 pass
+
+    def setcookie(self,cookiestring):
+        cookie = {}
+        if cookiestring is not None:
+            for onecookiestring in cookiestring.split(";"):
+                a, b = onecookiestring.split("=")
+                cookie.update({a: b})
+            self.s.cookies.update(cookie)
 
     def showcookie(self):
         """
@@ -76,9 +81,9 @@ class EasyLogin:
         for i in self.s.cookies:
             c += i.name + '=' + i.value + ";"
         return c
-    cookie=property(showcookie)
+    cookie = property(showcookie)
 
-    def get(self, url, result=True, save=False, headers=None, o=False, cache=None):
+    def get(self, url, result=True, save=False, headers=None, o=False, cache=None, r=False, cookiestring=None,failstring=None):
         """
         HTTP GET method, default save soup to self.b
         :param url: a url, example: "http://ip.cn"
@@ -86,9 +91,12 @@ class EasyLogin:
         :param save: save cookie or not
         :param headers: more headers to be sent
         :param o: return object or just page text
-        :param cache: filename to write cache, if already exists, use cache rather than really get
+        :param cache: filename to write cache, if already exists, use cache rather than really get; using cache=True to use md5(url) as cache file name
+        :param failstring: if failstring occurs in text, raise an exception
         :return page text or object(o=True)
         """
+        if cache is True:
+            cache = mymd5(url)
         if cache is not None and os.path.exists(cache):
             if o:
                 obj = pickle.load(open(cache, "rb"))
@@ -99,8 +107,21 @@ class EasyLogin:
                 page = open(cache, "rb").read()
                 if result:
                     self.b = BeautifulSoup(page.replace(b"<br>", b"\n").replace(b"<BR>", b"\n"), 'html.parser')
-                return page.decode()
+                return page.decode(errors='replace')
+        if r:
+            if headers is None:
+                headers = {"Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "zh-CN,zh;q=0.8", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Upgrade-Insecure-Requests": "1", "DNT": "1"}
+            headers["Referer"] = "/".join(url.split("/")[:3])
+        if cookiestring is not None:
+            if headers is None:
+                headers = {}
+            headers["Cookie"] = cookiestring
         x = self.s.get(url, headers=headers, allow_redirects=False, proxies=self.proxies)
+        if failstring is not None:
+            class EasyLogin_ValidateFail:
+                pass
+            if failstring in x.text:
+                raise EasyLogin_ValidateFail
         if result:
                 page = x.content.replace(b"<br>", b"\n").replace(b"<BR>", b"\n")
                 self.b = BeautifulSoup(page, 'html.parser')
@@ -115,7 +136,7 @@ class EasyLogin:
                 open(cache, "wb").write(x.content)
             return x.text
 
-    def post(self, url, data, result=True, save=False, headers=None):
+    def post(self, url, data, result=True, save=False, headers=None,cache=None):
         """
         HTTP POST method, submit data to server
         :param url: post target url
@@ -123,8 +144,16 @@ class EasyLogin:
         :param result: the page returned save to a.b
         :param save: save cookie to file
         :param headers: override headers to be sent
+        :param cache: filename to write cache, if already exists, use cache rather than really get; using cache=True to use md5(url+data) as cache file name
         :return: the requests object
         """
+        if cache is True:
+            cache = mymd5(url+data)
+        if cache is not None and os.path.exists(cache):
+                obj = pickle.load(open(cache, "rb"))
+                if result:
+                    self.b = BeautifulSoup(obj.content.replace(b"<br>", b"\n").replace(b"<BR>", b"\n"), 'html.parser')
+                return obj
         postheaders = headers if headers is not None else \
             {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
         x = self.s.post(url, data, headers=postheaders, allow_redirects=False, proxies=self.proxies)
@@ -133,9 +162,11 @@ class EasyLogin:
             self.b = BeautifulSoup(page, 'html.parser')
         if save:
             open(self.cookiefile, "wb").write(pickle.dumps(self.s.cookies))
+        if cache is not None:
+            open(cache, "wb").write(pickle.dumps(x))
         return x
 
-    def post_dict(self, url, dict, result=True, save=False, headers=None):
+    def post_dict(self, url, dict, result=True, save=False, headers=None,cache=None):
         """
         like post but using dict to post
         :param url: post target url
@@ -145,8 +176,25 @@ class EasyLogin:
         :param headers: override headers to be sent
         :return: the requests object
         """
+        if cache is True:
+            dict = OrderedDict(sorted(dict.items(), key=lambda t: t[0]))
         data = urlencode(dict)
-        return self.post(url, data, result=result, save=save, headers=headers)
+        return self.post(url, data, result=result, save=save, headers=headers,cache=cache)
+
+    def post_json(self, url, jsondata, result=False, save=False, headers=None, cache=None, o=False):
+        """
+        add a header for json post
+        :return the object
+        """
+        if headers is None:
+            headers={}
+        headers["content-type"]="application/json;charset=UTF-8"
+        data=json.dumps(jsondata)
+        x=self.post(url, data, result=result, save=save, headers=headers,cache=cache)
+        if o:
+            return x
+        else:
+            return x.json()
 
     def f(self, name, attrs):
         """
@@ -155,7 +203,8 @@ class EasyLogin:
         :param attrs: dict, exmaple: {"id":"content"}
         :return: list of str(Tag text)
         """
-        if self.b is None: return []
+        if self.b is None:
+            return []
         return [i.text.replace('\r', '').replace('\n', '').replace('\t', '').replace('  ', '')
                 for i in self.b.find_all(name, attrs)]
 
@@ -188,22 +237,24 @@ class EasyLogin:
     getList = getlist
 
     def img(self):
-        return [i[2:] if i[0:2]=="//" else i for i in self.getlist("","img","src")]
+        return [i[2:] if i[0:2] == "//" else i for i in self.getlist("", "img", "src")]
 
     def css(self):
-        return [i[2:] if i[0:2] == "//" else i for i in self.getlist("css","link","href")]
+        return [i[2:] if i[0:2] == "//" else i for i in self.getlist("css", "link", "href")]
 
     def js(self):
-        return [i[2:] if i[0:2] == "//" else i for i in self.getlist("js","script","src")]
+        return [i[2:] if i[0:2] == "//" else i for i in self.getlist("js", "script", "src")]
 
     def VIEWSTATE(self):
         """
         Useful when you crack the ASP>NET website
         :return: quoted VIEWSTATE str
         """
-        if self.b is None: return ""
+        if self.b is None:
+            return ""
         x = self.b.find("input", attrs={"name": "__VIEWSTATE"})
-        if x is None: return ""
+        if x is None:
+            return ""
         return quote(x["value"])
 
     def save(self, filename="EasyLogin.status"):
@@ -213,11 +264,11 @@ class EasyLogin:
         :param filename:
         :return:
         """
-        b=self.b
-        self.b=None
-        data=pickle.dumps(self)
+        b = self.b
+        self.b = None
+        data = pickle.dumps(self)
         open(filename, "wb").write(data)
-        self.b=b
+        self.b = b
         return
 
     def load(filename='EasyLogin.status'):
@@ -258,13 +309,74 @@ class EasyLogin:
         from bs4.element import NavigableString
         result = []
         for descendant in target.descendants:
-            if not isinstance(descendant, NavigableString) or descendant.parent.name == "script" or isinstance(
-                    descendant, Comment):
+            if not isinstance(descendant, NavigableString) \
+                    or descendant.parent.name in ["script", "style"] \
+                    or isinstance(descendant, Comment) \
+                    or "none" in descendant.parent.get("style","")\
+                    or "font-size:0px" in descendant.parent.get("style",""):
                 continue
             data = descendant.strip()
             if len(data) > 0:
                 result.append(data)
         return result
+
+    def find(self, tag, attrs_string, skip=0, text=False):
+        """
+        find more easily with string, return all matched tag
+        :param tag: tag name
+        :param attrs_string: tag attrs, fully match
+        :param skip: skip first tags
+        :param text: need text or tag
+        :return: array of tag or text
+        """
+        tmp_tag = BeautifulSoup("""<%s %s></%s>""" % (tag, attrs_string, tag), "html.parser").find(tag)
+
+        def mysearch(itag):
+            if itag.name != tag:
+                return False
+            if itag.attrs == tmp_tag.attrs:
+                return True
+            else:
+                return False
+        data = self.b.find_all(mysearch)
+        for i in range(skip):
+            if not len(data):
+                break
+            del(data[0])
+        if text:
+            return [self.text(i) for i in data]
+        else:
+            return data
+
+    def d(self,tag,attrs,all=False):
+        """
+        delete some useless tags
+        :param tag: tag name
+        :param attrs: tag attrs
+        :param all: delete all matches or just the first one
+        :return: False when not found any matches
+        """
+        if self.b is None:
+            return False
+        tags=self.b.find_all(tag,attrs=attrs)
+        if len(tags)==0:
+            return False
+        if all == False:
+            tags = tags[0:1]
+        for tag in tags:
+            tag.extract()
+        return True
+
+    @staticmethod
+    def safefilename(filename):
+        """
+        convert a string to a safe filename
+        :param filename: a string, may be url or name
+        :return: special chars replaced with _
+        """
+        for i in "\\/:*?\"<>|$":
+            filename=filename.replace(i,"_")
+        return filename
 
 
 if __name__ == '__main__':  # sample code for get ip by "http://ip.cn"
